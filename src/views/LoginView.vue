5ed12b54d576434f0d0126e57d8d53a3f974eca0
ramadikaa <muhamadramadika3@gmail.com>
2024-10-24 14:46:41 +0700

    login perusahaan

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,14 @@
 <script setup>
 import MainWrapper from '@/components/MainWrapper.vue'
+import { ref } from 'vue'
+
+// Reactive variable to toggle password visibility
+const showPassword = ref(false)
+
+// Function to toggle the password visibility
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value
+}
 </script>
 
 <template>
@@ -9,7 +18,7 @@ import MainWrapper from '@/components/MainWrapper.vue'
         <div class="row align-items-center">
           <div class="col-lg-12">
             <div class="login-logo">
-              <img src="@/assets/img/logo-login.png" alt="img" />
+              <img src="@/assets/img/dlh.jpg" alt="img" style="width: 200px; height: auto;" />
             </div>
           </div>
           <div class="col-lg-6 col-xl-7">
@@ -20,8 +29,8 @@ import MainWrapper from '@/components/MainWrapper.vue'
           <div class="col-lg-6 col-xl-5">
             <div class="login-content">
               <div class="login-contenthead">
-                <h5>Login</h5>
-                <h6>We'll send a confirmation code to your email.</h6>
+                <h5>Login Perusahaan</h5>
+                <h6>Masukan Email dan Password Anda.</h6>
               </div>
               <div class="login-input">
                 <div class="form-group">
@@ -35,33 +44,30 @@ import MainWrapper from '@/components/MainWrapper.vue'
                 <div class="form-group">
                   <div class="d-flex justify-content-between">
                     <label>Password</label>
-                    <a class="forgetpassword-link" href="forget-password.html"
-                      >Forgot password?</a
-                    >
                   </div>
                   <div class="pass-group">
                     <input
-                      type="password"
+                      :type="showPassword ? 'text' : 'password'"
                       class="form-control pass-input"
                       placeholder="********"
                     />
-                    <span class="fas toggle-password fa-eye-slash"></span>
+                    <span 
+                      class="fas toggle-password"
+                      :class="showPassword ? 'fa-eye' : 'fa-eye-slash'"
+                      @click="togglePasswordVisibility"
+                    ></span>                    
                   </div>
-                </div>
-                <div class="filter-checkbox m-0">
-                  <ul class="d-flex justify-content-between">
-                    <li>
-                      <label class="checkboxs">
-                        <input type="checkbox" />
-                        <span><i></i></span>
-                        <b class="check-content">Remember Me</b>
-                      </label>
-                    </li>
-                  </ul>
                 </div>
               </div>
               <div class="login-button">
                 <a href="index.html" class="btn btn-login">Login</a>
+              </div>
+              <!-- Forgot password and sign up section -->
+              <div class="login-footer mt-3 text-left">
+                <a href="forget-password.html" class="d-block mb-2">Forgot password?</a>
+                <span>Belum punya akun perusahaan? 
+                  <a href="sign-up.html" class="signup-link">Sign Up</a>
+                </span>
               </div>
             </div>
           </div>
