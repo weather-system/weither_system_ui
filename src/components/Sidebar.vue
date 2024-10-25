26c231a5a30b831904c3751429795e478f9a43cb
rizkyayahyaa <rizkyayahya13@gmail.com>
2024-10-25 23:46:55 +0700

    add template page pengendalian

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -9,11 +9,10 @@ const isTiketOpen = ref(false)
 
 // Function to toggle the visibility of the submenus
 const togglePengendalian = () => {
-  isPengendalianOpen.value = !isPengendalianOpen.value
-  isLogbookOpen.value = false
-  isImportLogbookOpen.value = false
-  isTiketOpen.value = false
-}
+  console.log("Pengendalian clicked"); // Debugging log
+  isPengendalianOpen.value = !isPengendalianOpen.value;
+};
+
 
 const toggleLogbook = () => {
   isLogbookOpen.value = !isLogbookOpen.value
@@ -41,10 +40,10 @@ const toggleTiket = () => {
   <div class="sidebar" id="sidebar">
     <div class="sidebar-header">
       <div class="sidebar-logo">
-        <a href="index.html">
+        <a href="/">
           <img src="@/assets/img/logo.svg" class="img-fluid logo" alt="" />
         </a>
-        <a href="index.html">
+        <a href="/">
           <img
             src="@/assets/img/logo-small.svg"
             class="img-fluid logo-small"
@@ -67,55 +66,72 @@ const toggleTiket = () => {
             <h6>Home</h6>
           </li>
           <li>
-            <a href="index.html" class="active">
+            <router-link to="/" class="active">
               <i class="fe fe-grid"></i> <span>Dashboard</span>
-            </a>
+            </router-link>
           </li>
+
           <li>
             <a href="javascript:void(0);" @click="togglePengendalian">
               <i class="fe fe-briefcase"></i> <span>Pengendalian</span>
-              <i class="fe" :class="{'fe-chevron-down': !isPengendalianOpen, 'fe-chevron-up': isPengendalianOpen}"></i>
+              <i class="fe" :class="{
+                'fe-chevron-down': !isPengendalianOpen,
+                'fe-chevron-up': isPengendalianOpen
+              }"></i>
             </a>
             <ul v-if="isPengendalianOpen" class="d-block ms-4">
-              <li><a href="sub-service1.html">Pencemaran Air</a></li>
-              <li><a href="sub-service2.html">Pencemaran Udara</a></li>
-              <li><a href="sub-service3.html">Pengelolaan Limbah B3</a></li>
+              <li>
+                <router-link to="/pengendalian/air">Pencemaran Air</router-link>
+              </li>
+              <li>
+                <router-link to="/pengendalian/udara">Pencemaran Udara</router-link>
+              </li>
+              <li>
+                <router-link to="/pengendalian/limbah-b3">Pengelolaan Limbah B3</router-link>
+              </li>
             </ul>
           </li>
+
           <li>
             <a href="javascript:void(0);" @click="toggleLogbook">
               <i class="fe fe-briefcase"></i> <span>Logbook</span>
-              <i class="fe" :class="{'fe-chevron-down': !isLogbookOpen, 'fe-chevron-up': isLogbookOpen}"></i>
+              <i class="fe" :class="{
+                'fe-chevron-down': !isLogbookOpen,
+                'fe-chevron-up': isLogbookOpen
+              }"></i>
             </a>
             <ul v-if="isLogbookOpen" class="d-block ms-4">
-              <li><a href="sub-service1.html">Produksi Senyatanya</a></li>
-              <li><a href="sub-service1.html">Pemakaian Bahan Kimia</a></li>
-              <li><a href="sub-service1.html">Pemakaian Air</a></li>
-              <li><a href="sub-service1.html">Debit Outlet IPAL</a></li>
-              <li><a href="sub-service1.html">Debit Pemakaian Air</a></li>
-              <li><a href="sub-service1.html">IPAL</a></li>
-              <li><a href="sub-service1.html">Penggunaan B3</a></li>
-              <li><a href="sub-service2.html">TPS Limbah B3</a></li>
+              <li><router-link to="/logbook/produksi">Produksi Senyatanya</router-link></li>
+              <li><router-link to="/logbook/kimia">Pemakaian Bahan Kimia</router-link></li>
+              <li><router-link to="/logbook/air">Pemakaian Air</router-link></li>
             </ul>
           </li>
+
           <li>
             <a href="javascript:void(0);" @click="toggleImportLogbook">
               <i class="fe fe-briefcase"></i> <span>Import Logbook</span>
-              <i class="fe" :class="{'fe-chevron-down': !isImportLogbookOpen, 'fe-chevron-up': isImportLogbookOpen}"></i>
+              <i class="fe" :class="{
+                'fe-chevron-down': !isImportLogbookOpen,
+                'fe-chevron-up': isImportLogbookOpen
+              }"></i>
             </a>
             <ul v-if="isImportLogbookOpen" class="d-block ms-4">
-              <li><a href="sub-service1.html">Logbook IPAL</a></li>
-              <li><a href="sub-service2.html">Logbook Limbah B3</a></li>
+              <li><router-link to="/import-logbook/ipal">Logbook IPAL</router-link></li>
+              <li><router-link to="/import-logbook/limbah-b3">Logbook Limbah B3</router-link></li>
             </ul>
           </li>
+
           <li>
             <a href="javascript:void(0);" @click="toggleTiket">
               <i class="fe fe-briefcase"></i> <span>Tiket</span>
-              <i class="fe" :class="{'fe-chevron-down': !isTiketOpen, 'fe-chevron-up': isTiketOpen}"></i>
+              <i class="fe" :class="{
+                'fe-chevron-down': !isTiketOpen,
+                'fe-chevron-up': isTiketOpen
+              }"></i>
             </a>
             <ul v-if="isTiketOpen" class="d-block ms-4">
-              <li><a href="sub-service1.html">Daftar Pengaduan</a></li>
-              <li><a href="sub-service2.html">Pengaduan</a></li>
+              <li><router-link to="/tiket/pengaduan">Daftar Pengaduan</router-link></li>
+              <li><router-link to="/tiket/pengaduan-baru">Pengaduan</router-link></li>
             </ul>
           </li>
         </ul>
@@ -123,3 +139,4 @@ const toggleTiket = () => {
     </div>
   </div>
 </template>
+
---
+++ src/views/pengendalian/PencemaranAir.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { ref } from 'vue'
+import MainWrapper from '@/components/MainWrapper.vue'  // Import MainWrapper
+
+const airData = ref([
+  { id: 1, lokasi: 'Danau Toba', kualitas: 'Baik', parameter: 'pH, DO', tanggal: '2024-10-20' },
+  { id: 2, lokasi: 'Sungai Citarum', kualitas: 'Buruk', parameter: 'BOD, COD', tanggal: '2024-10-21' },
+])
+</script>
+
+<template>
+  <MainWrapper>
+    <div class="page-wrapper">
+      <div class="content">
+        <div class="content-page-header content-page-headersplit">
+          <h5>Pengendalian Pencemaran Air</h5>
+          <div class="list-btn">
+            <ul>
+              <li>
+                <a class="btn btn-primary" href="add-water-quality.html">
+                  <i class="fa fa-plus me-2"></i>Tambah Kualitas Air
+                </a>
+              </li>
+              <li>
+                <div class="filter-sorting">
+                  <ul>
+                    <li>
+                      <a href="javascript:void(0);" class="filter-sets">
+                        <img src="@/assets/img/icons/filter1.svg" class="me-2" alt="img" />Filter
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="table-responsive table-div">
+          <table class="table datatable table-bordered mt-3">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Lokasi</th>
+                <th>Kualitas Air</th>
+                <th>Parameter</th>
+                <th>Tanggal Pengukuran</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(data, index) in airData" :key="data.id">
+                <td>{{ index + 1 }}</td>
+                <td>{{ data.lokasi }}</td>
+                <td>{{ data.kualitas }}</td>
+                <td>{{ data.parameter }}</td>
+                <td>{{ data.tanggal }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </MainWrapper>
+</template>
+
+<style scoped>
+.table {
+  width: 100%;
+  border-collapse: collapse;
+}
+.table th, .table td {
+  padding: 10px;
+  text-align: left;
+  border: 1px solid #ddd;
+}
+</style>
