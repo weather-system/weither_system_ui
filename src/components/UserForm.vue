5ad749de4aa99db9a358645a5747a272bd034fef
estradax <azharumuhammad@gmail.com>
2024-11-10 17:52:18 +0700

    update css and add crud user

---
+++ src/components/UserForm.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref } from 'vue';
+import { Form, Field, ErrorMessage, FieldArray } from 'vee-validate'
+import * as yup from 'yup'
+
+const form = ref(null)
+
+const schema = yup.object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string(),
+    role: yup.string().required()
+})
+
+const setValues = (data) => {
+  form.value.setValues(data)
+}
+
+defineExpose({ setValues })
+</script>
+
+<template>
+  <Form ref="form" :validation-schema="schema">
+    <div class="row">
+      <div class="mt-2">
+      <div class="col-4">
+          <label class="form-label">Name</label>
+          <Field name="name" class="form-control" />
+          <ErrorMessage name="name" />
+      </div>
+      <div class="col-4">
+          <label class="form-label">Email</label>
+          <Field name="email" class="form-control" />
+          <ErrorMessage name="email" />
+      </div>
+      <div class="col-4">
+          <label class="form-label">Password</label>
+          <Field name="password" class="form-control" type="password" />
+          <ErrorMessage name="password" />
+      </div>
+      <div class="col-4">
+          <label class="form-label">Role</label>
+          <Field name="role" class="form-control" as="select">
+            <option value="ADMIN">ADMIN</option>
+            <option value="USER">USER</option>
+            <option value="EKSEKUTIF">EKSEKUTIF</option>
+          </Field>
+          <ErrorMessage name="role" />
+      </div>
+      <div class="mt-4">
+          <button class="btn btn-primary">Simpan</button>
+        </div>
+      </div>
+    </div>
+  </Form>
+</template>
---
+++ src/views/Master/UserEditView.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+import { useLoading } from 'vue-loading-overlay';
+import MainWrapper from '@/components/MainWrapper.vue'
+import UserForm from '@/components/UserForm.vue';
+import { getUserDetail, updateUser } from '@/lib/user.js';
+
+const $loading = useLoading()
+const router = useRouter()
+const route = useRoute()
+
+const form = ref(null)
+
+const submit = async (data) => {
+  const loader = $loading.show()
+  try {
+    await updateUser(route.params.id, data)
+    router.push('/Master/User')
+  } catch (e) {
+    console.error(e)
+  } finally {
+    loader.hide()
+  }
+}
+
+onMounted(async () => {
+  const loader = $loading.show()
+  try {
+    const data = await getUserDetail(route.params.id)
+    form.value.setValues(data)
+  } catch (e) {
+    console.error(e)
+  } finally {
+    loader.hide()
+  }
+})
+</script>
+
+<template>
+  <MainWrapper>
+    <div class="page-wrapper page-settings">
+      <div class="content">
+        <div class="content-page-header mb-2">
+          <h3>Edit User</h3>
+        </div>
+
+        <UserForm ref="form" @submit="submit" />
+      </div>
+    </div>
+  </MainWrapper>
+</template>
